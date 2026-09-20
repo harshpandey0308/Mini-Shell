c1787c9ae721de4485e66398a3fffd0d9e66f877
Harshpandey <208502446+harshpandey0308@users.noreply.github.com>
2026-09-20 03:28:47 +0000

    Updated

--- receiver.c
+++ receiver.c
@@ -1,17 +1,46 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<unistd.h>
+#include<sys/ipc.h>
 #include<sys/msg.h>
 
+#define MAX_SIZE 100
+
+typedef struct{
+    long mtype;
+    char mtext[MAX_SIZE];
+}msgbuf;
+
 int main(){
+    msgbuf msgp;
+
     key_t key = ftok("/tmp" , 'A');
+
+    if(key == -1){
+        perror("ftok failed");
+        exit(EXIT_FAILURE);
+    }
 
     int id = msgget(key , 0666);
 
     if(id == -1){
-        printf("Queue creation is unsuccessfull.\n");
-        return 1;
+        perror("the msgget is not successfull");
+        exit(EXIT_FAILURE);
     }
     else{
         printf("Queue created successfully and queue id is : %d\n",id);
+         
+        size_t msg_recv = msgrcv(id , &msgp , sizeof(msgp.mtext) , 0 , 0);
+
+        if(msg_recv == -1){
+          perror("message received failed");
+          exit(EXIT_FAILURE);
+        }
+        else{
+            printf("Message received : %s" , msgp.mtext);
+        }
+
     }
+
+    return 0;
 }
--- sender.c
+++ sender.c
@@ -1,18 +1,47 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include<unistd.h>
 #include<sys/msg.h>
 
+#define MAX_SIZE 100
+
+typedef struct{
+    long mtype;
+    char mtext[MAX_SIZE];
+}msgbuf;
+
 int main(){
+    size_t msgsz;
+
+    msgbuf msgp;
+
+    msgp.mtype = 1;
+    strcpy(msgp.mtext , "hello , mq");
+
+    msgsz = strlen(msgp.mtext);
+
     key_t key = ftok("/tmp" , 'A');
 
-    int id = msgget(key , IPC_CREAT | 0666);
+    if(key == -1){
+        perror("ftok failed , no such process exist");
+        exit(EXIT_FAILURE);
+    }
+    
+    int msgqid = msgget(key , IPC_CREAT | 0666);
 
-    if(id == -1){
-        printf("Queue creation failed.\n");
-        return 1;
+    if(msgqid == -1){
+        perror("msgget() failed ");
+        exit(EXIT_FAILURE);
     }
     else{
-        printf("The Queue created successfully and the queue id is %d.\n",id);
+        printf("the queue found successfully and the qid is %d.\n",msgqid);
+       
+        int snd_flg = msgsnd(msgqid , &msgp , msgsz , 0);
+
+        if(snd_flg == 0){
+            printf("the message send successfully.\n");
+        }
     }
 
     return 0;
